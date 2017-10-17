df016f6539acac65da304e8084c5461cb51dcc94
DragOnMe <dragonme@gmail.com>
2017-10-17 21:32:52 +0900

    Add go binary build & copy commands

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# Copy demo-multicast go binary
+# Build & Copy demo-multicast go binary
+go install $GOPATH/multicast.go
 cp $GOBIN/multicast ./multicast_linux_amd64
 
 gox -osarch="linux/amd64"
