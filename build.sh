cc45e1f56a3fe316240cfda60fe5ad85b2bf03f5
DragOnMe <dragonme@gmail.com>
2017-10-28 18:53:28 +0900

    mod build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Build & Copy demo-multicast go binary
+go install $GOPATH/multicast.go
+cp $GOBIN/multicast ./multicast_linux_amd64
+
 gox -osarch="linux/amd64"
 
 docker build -t drlee001/demo-multicast .
